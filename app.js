6be7353133fa6638f27db0fcf9b3e516ac47a9be
dave <davemcelhaney@gmail.com>
2022-07-20 22:38:22 -0400

    added function to app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('js yo')
+function randomColor () {
+    const r = Math.floor(Math.random * 256)
+    const g = Math.floor(Math.random * 256)
+    const b = Math.floor(Math.random * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+const letters = document.querySelector('h1');
+const intervalId = setinterval(function() {
+    for (let letter of letters) {
+        letter.style.color = randomColor();
+    }
+}, 1000);

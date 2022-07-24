14f2b7662dd7477e43125a607f5fc5fb15dc0962
dave <davemcelhaney@gmail.com>
2022-07-24 13:45:23 -0400

    purple gone

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function randomColor () {
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
     const b = Math.floor(Math.random() * 256)
-    return `rgb(${r}, ${g}, ${b})`
+    return `rgb(${r}, 0, ${b})`
 }
 
 const body = document.querySelector('body')

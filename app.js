d4cf98d47c7935a5895038dfcb80e9afe6498821
dave <davemcelhaney@gmail.com>
2022-07-24 21:28:57 -0400

    trying to push

---
+++ app.js
@@ -0,0 +1,18 @@
+function randomColor () {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, 0, ${b})`
+}
+
+const body = document.querySelector('body')
+const letters = document.querySelectorAll('h1');
+const intervalId = setInterval(function() {
+    for (let letter of letters) {
+        letter.style.color = randomColor();
+    }
+}, 1000);
+
+const bgInterval = setInterval(function() {
+    body.style.backgroundColor = randomColor();
+}, 1000)
